602e7ffeca38c03cef0182c6d0dabc321c9a49ae
Gustavo <gustavotremontsr@gmail.com>
2021-12-14 18:02:13 +0100

    miss files

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Header from './components/Header';
 import Footer from './components/Footer'
 import Main from './components/Main'
 
+import {BrowserRouter} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
+      <BrowserRouter>      
+        <Header/>
+        <Main/>
+      </BrowserRouter>
       <Footer/>
     </div>
   );
